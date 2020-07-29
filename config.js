fc14ceef6d762b376cce750e392d2aff5bcf35c7
Abhijeet <abhijeet.millenium@gmail.com>
2020-07-28 20:43:09 -0600

    Implemented basic structure

--- config.js
+++ config.js
@@ -8,8 +8,247 @@ module.exports = {
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
   pathPrefix: `/gatsby-starter-spectral/`, // This path is subpath of your hosting https://domain/portfolio
-  heading: 'Anubhav Srivastava',
-  subHeading: 'Full time Web Developer. Part time Open source contributor  ',
+  heading: 'Abhijeet Srivastava',
+  subHeading: `<Full Stack Developer/>`,
+  about: "I am proficient in Test Driven Development of Web Applications. Designed and developed Single Page Applications " +
+      "using JavaScript, HTML5, CSS3, ReactJS, Redux and TypeScript. Unit and Integration tests using Jest, Enzyme and Cypress. " +
+      "Designed and developed JSON-based REST services using Java Spring Boot.",
+
+  highlights: [
+    "13 years of experience in System Development Life Cycle (SDLC) including Requirement Analysis, Design, Development & Testing.",
+    "Designed and develop reusable component based UI using latest web frameworks and design patterns.",
+    "Proposed solutions for business requirements designed and developed proof of concept prototype for advanced features and designs.",
+    "Involved in development following UI standards, coding guidelines and process to ensure high quality deliverables via design reviews and code reviews and unit test coverage.",
+    "Experience in continuous integration tools like Jenkins, client side build dependency management tools such as npm, yarn etc.",
+    "Anchored various application features and led the effort ensuring end-to-end delivery.",
+    "Participated in sprint planning, scrum ceremonies, sprint demos, User Acceptance Testing and release planning.",
+    "Work collaboratively in an Agile Environment."
+  ],
+
+  education: [
+    {
+      university: "U.P. Technical University, Lucknow",
+      degree: "Master of Computer Application",
+      year: "2002 - 2005"
+    },
+    {
+      university: "Purvanchal University, Jaunpur",
+      degree: "Bachelor of Science",
+      year: "1999 - 2002"
+    }
+  ],
+
+  experience: [
+    {
+      client: "Dish Network",
+      role: "Full Stack Developer",
+      year: "August 2018 - June 2020",
+      description: "• Developed full stack stories using ReactJS, Redux, TypeScript and Java Spring Boot.\n" +
+          "• Developed reusable UI components in React JS.\n" +
+          "• Involved in designing application responsive layout to refine user experience on different devices using CSS3, Flexbox and Media Query.\n" +
+          "• Developed backend services using Spring Boot Framework.\n" +
+          "• Followed Test Driven Development and created Unit and Integration test cases using Jest, Enzyme, Cypress JUnit and Mockito.\n" +
+          "• Worked with Product Owner and business team for feature mapping and converting to stories."
+    },
+    {
+      client: "Credit Suisse",
+      role: "Front End Developer",
+      year: "May 2017 - August 2018",
+      description: "• Developed client side presentation using JavaScript, ReactJS and Redux.\n" +
+          "• Designed and developed various reusable presentation components in React JS like Tooltip, Drop Down, Accordion, Typeahead Input Box.\n" +
+          "• Created form validator framework. Any form wrapped inside validator is validated on submission.\n" +
+          "• Created test cases and implemented unit testing using Jest, Enzyme.\n" +
+          "• Occasionally wrote migration files in Liquibase for any DB related change.\n" +
+          "• Occasionally Wrote Java services to serve any front-end requests.\n" +
+          "• Worked with Service team to define the contracts for end points.\n" +
+          "• Worked with Visual design team to set the design guidelines."
+    },
+    {
+      client: "Walmart eCommerce",
+      role: "Front End Developer",
+      year: "June 2014 - March 2017",
+      description: "• Provided support for maintenance and enhancement for Electrode and Atlas platform for all the tracks including Home page, Item Page, Category/Browse Page and CXO.\n" +
+          "• Responsible for troubleshooting issues and suggesting developer team about required code change.\n" +
+          "• Identifying issues related to items in various stages like ingestion, setup, item content, pricing by validating underlying services response involved like IRO, item store, item pricing, Preso, IDML, MIDAS etc.\n" +
+          "• Managing CCM client configuration for controlling various sections of the pages on the site like enable/disable the module, changing SLA for services response time etc.\n" +
+          "• Involved in report creation & scheduling in Splunk for error analysis.\n" +
+          "• Customer Experience & business impact analysis through IBM Tealeaf tool for triaging and resolving production issues.\n" +
+          "• Involved in Release, Deployment verification in OneOps.\n" +
+          "• Developed a dashboard in HTML5, CSS3, Bootstrap and AngularJS to facilitate support team in troubleshooting issues related to items on Wal-Mart site. This feature provides the merchants to get the current status of any item such as its availability in Pangaea world, whether it’s searchable on site, IRO service status etc. User can export the customized form of the result in CSV format."
+    },
+    {
+      client: "Bank of New York Mellon",
+      role: "Flex Developer",
+      year: "January 2011 - December 2013",
+      description: "• Analysis, Design, development and testing of software modules.\n" +
+          "• Reviewing test results and documenting test results,\n" +
+          "• Coordination with onsite/clients."
+    },
+    {
+      client: "Genpact Headstrong Capital Markets",
+      role: "Flex Developer",
+      year: "May 2008 - December 2010",
+      description: "• GUI design and development of various modules like patient registration, diagnosis, discharge, EMR etc. using Adobe Flex Cairngorm framework.\n" +
+          "• Component designing like keyboard and calculator.\n" +
+          "• Design and development of java services for various modules using Hibernate ORM solution."
+    },
+    {
+      client: "CDAC, Department of Science & Technology, Government of India",
+      role: "Java Developer",
+      year: "July 2007 - May 2008",
+      description: "• GUI design and development using SWING and AWT components.\n" +
+          "• UNICODE handling.\n" +
+          "• File processing like file I/O etc.\n" +
+          "• Unit and integration testing of modules."
+    },
+    {
+      client: "National Institute of Science Communication& Information Resources (NISCAIR), CSIR",
+      role: "Java Developer",
+      year: "May 2006 - July 2007",
+      description: "Involve in designing screen in HTML, JavaScript, AJAX, coding in JSP, servlet and it’s testing."
+    }
+
+  ],
+
+  skills: {
+    language: [
+      {
+        id: "html5",
+        value: "HTML5",
+      },
+      {
+        id: "css3",
+        value: "CSS3",
+      },
+      {
+        id: "javascript",
+        value: "JavaScript",
+      },
+      {
+        id: "es6",
+        value: "ES6",
+      },
+      {
+        id: "typescript",
+        value: "TypeScript",
+      },
+      {
+        id: "javaj2ee",
+        value: "Java/J2EE"
+      },
+      {
+        id: "nodejs",
+        value: "NodeJS",
+      }
+    ],
+
+    technology: [
+      {
+        id: "jest",
+        value: "Jest",
+      },
+      {
+        id: "enzyme",
+        value: "Enzyme",
+      },
+      {
+        id: "cypress",
+        value: "Cypress",
+      },
+      {
+        id: "webpack",
+        value: "Webpack",
+      },
+      {
+        id: "materialUI",
+        value: "Material-UI",
+      },
+      {
+        id: "bootstrap",
+        value: "Bootstrap"
+      },
+      {
+        id: "highchart",
+        value: "High chart",
+      },
+      {
+        id: "yarn",
+        value: "Yarn",
+      },
+      {
+        id: "npm",
+        value: "NPM"
+      }
+    ],
+
+    framework: [
+      {
+        id: "reactjs",
+        value: "ReactJS",
+      },
+      {
+        id: "redux",
+        value: "Redux",
+      },
+      {
+        id: "angularjs",
+        value: "Angular 1.x",
+      },
+      {
+        id: "springframework",
+        value: "Spring Framework",
+      },
+      {
+        id: "springboot",
+        value: "Spring Boot",
+      }
+    ],
+
+    tools: [
+      {
+        id: "intellij",
+        value: "IntelliJ",
+      },
+      {
+        id: "eclipse",
+        value: "Eclipse",
+      },
+      {
+        id: "github",
+        value: "GitHub",
+      },
+      {
+        id: "gitlab",
+        value: "GitLab",
+      },
+      {
+        id: "svn",
+        value: "SVN",
+      },
+      {
+        id: "jira",
+        value: "JIRA",
+      },
+      {
+        id: "rally",
+        value: "Rally",
+      },
+      {
+        id: "plantuml",
+        value: "PlantUML",
+      },
+      {
+        id: "postman",
+        value: "Postman",
+      },
+      {
+        id: "chromedevtools",
+        value: "Chrome DevTools",
+      }
+    ]
+
+  },
+
   // social
   socialLinks: [
     {
@@ -31,4 +270,19 @@ module.exports = {
       url: 'mailto:test@example.com',
     },
   ],
+
+  certification: [
+    {
+      id: 'scjp',
+      name: 'Sun Certified Programmer for the Java Platform, Standard Edition 5.0 (SCJP)'
+    },
+    {
+      id: 'scwcd',
+      name: 'Sun Certified Web Component Developer for the Java Platform, Enterprise Edition 5 (SCWCD)'
+    },
+    {
+      id: 'splunk',
+      name: 'Using Splunk 6 - Dedicated Virtual'
+    }
+  ]
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Layout from '../components/Layout';
 import Scroll from '../components/Scroll';
 
 import pic1 from '../assets/images/pic01.jpg';
-import pic2 from '../assets/images/pic02.jpg';
+import pic2 from '../assets/images/avatar.png';
 import pic3 from '../assets/images/pic03.jpg';
 import config from '../../config';
 const IndexPage = () => (
@@ -14,190 +14,225 @@ const IndexPage = () => (
       <div className="inner">
         <h2>{config.heading}</h2>
         <p>{config.subHeading}</p>
-        <ul className="actions special">
-          <li>
-            <Scroll type="id" element="one">
-              <a href="/#" className="button primary">
-                Explore
-              </a>
-            </Scroll>
-          </li>
-        </ul>
       </div>
-      <Scroll type="id" element="one">
-        <a href="#one" className="more">
+      <Scroll type="id" element="two">
+        <a href="#two" className="more">
           Learn More
         </a>
       </Scroll>
     </section>
 
-    <section id="one" className="wrapper style1 special">
-      <div className="inner">
-        <header className="major">
-          <h2>
-            Arcu aliquet vel lobortis ata nisl
-            <br />
-            eget augue amet aliquet nisl cep donec
-          </h2>
-          <p>
-            Aliquam ut ex ut augue consectetur interdum. Donec amet imperdiet
-            eleifend
-            <br />
-            fringilla tincidunt. Nullam dui leo Aenean mi ligula, rhoncus
-            ullamcorper.
-          </p>
-        </header>
-        <ul className="icons major">
-          <li>
-            <span className="icon fa-gem major style1">
-              <span className="label">Lorem</span>
-            </span>
-          </li>
-          <li>
-            <span className="icon fa-heart major style2">
-              <span className="label">Ipsum</span>
-            </span>
-          </li>
-          <li>
-            <span className="icon solid fa-code major style3">
-              <span className="label">Dolor</span>
-            </span>
-          </li>
-        </ul>
-      </div>
-    </section>
+    {/*<section id="one" className="wrapper style1 special">*/}
+    {/*  <div className="inner">*/}
+    {/*    <header className="major">*/}
+    {/*      <h2>*/}
+    {/*        Arcu aliquet vel lobortis ata nisl*/}
+    {/*        <br />*/}
+    {/*        eget augue amet aliquet nisl cep donec*/}
+    {/*      </h2>*/}
+    {/*      <p>*/}
+    {/*        Aliquam ut ex ut augue consectetur interdum. Donec amet imperdiet*/}
+    {/*        eleifend*/}
+    {/*        <br />*/}
+    {/*        fringilla tincidunt. Nullam dui leo Aenean mi ligula, rhoncus*/}
+    {/*        ullamcorper.*/}
+    {/*      </p>*/}
+    {/*    </header>*/}
+    {/*    <ul className="icons major">*/}
+    {/*      <li>*/}
+    {/*        <span className="icon fa-gem major style1">*/}
+    {/*          <span className="label">Lorem</span>*/}
+    {/*        </span>*/}
+    {/*      </li>*/}
+    {/*      <li>*/}
+    {/*        <span className="icon fa-heart major style2">*/}
+    {/*          <span className="label">Ipsum</span>*/}
+    {/*        </span>*/}
+    {/*      </li>*/}
+    {/*      <li>*/}
+    {/*        <span className="icon solid fa-code major style3">*/}
+    {/*          <span className="label">Dolor</span>*/}
+    {/*        </span>*/}
+    {/*      </li>*/}
+    {/*    </ul>*/}
+    {/*  </div>*/}
+    {/*</section>*/}
 
     <section id="two" className="wrapper alt style2">
+      {/*<section className="spotlight">*/}
+      {/*  <div className="image">*/}
+      {/*    <img src={pic1} alt="" />*/}
+      {/*  </div>*/}
+      {/*  <div className="content">*/}
+      {/*    <h2>*/}
+      {/*      Magna primis lobortis*/}
+      {/*      <br />*/}
+      {/*      sed ullamcorper*/}
+      {/*    </h2>*/}
+      {/*    <p>*/}
+      {/*      Aliquam ut ex ut augue consectetur interdum. Donec hendrerit*/}
+      {/*      imperdiet. Mauris eleifend fringilla nullam aenean mi ligula.*/}
+      {/*    </p>*/}
+      {/*  </div>*/}
+      {/*</section>*/}
       <section className="spotlight">
-        <div className="image">
-          <img src={pic1} alt="" />
-        </div>
         <div className="content">
           <h2>
-            Magna primis lobortis
+            Hey, I'm Abhijeet.
             <br />
-            sed ullamcorper
           </h2>
+          <p>{config.about}</p>
+
           <p>
-            Aliquam ut ex ut augue consectetur interdum. Donec hendrerit
-            imperdiet. Mauris eleifend fringilla nullam aenean mi ligula.
+            <div>HIGHLIGHTS</div><br />
+            <ul>
+              {
+                config.highlights.map(item => <li>{item}</li>)
+              }
+            </ul>
           </p>
         </div>
-      </section>
-      <section className="spotlight">
         <div className="image">
           <img src={pic2} alt="" />
         </div>
-        <div className="content">
-          <h2>
-            Tortor dolore feugiat
-            <br />
-            elementum magna
-          </h2>
-          <p>
-            Aliquam ut ex ut augue consectetur interdum. Donec hendrerit
-            imperdiet. Mauris eleifend fringilla nullam aenean mi ligula.
-          </p>
-        </div>
       </section>
-      <section className="spotlight">
-        <div className="image">
-          <img src={pic3} alt="" />
-        </div>
+
+      <section className="experience-section">
+        {/*<div className="bg" data-anchor-target="#experience-section"*/}
+        {/*     data-bottom-top="transform: translate3d(0px, -150px, 0px);"*/}
+        {/*     data-top-bottom="transform: translate3d(0px, 150px, 0px);"*/}
+        {/*></div>*/}
+        <div className="overlay-wrapper">
         <div className="content">
-          <h2>
-            Augue eleifend aliquet
-            <br />
-            sed condimentum
-          </h2>
-          <p>
-            Aliquam ut ex ut augue consectetur interdum. Donec hendrerit
-            imperdiet. Mauris eleifend fringilla nullam aenean mi ligula.
-          </p>
+              <h1 className="section-heading">Education</h1>
+              <span className="divider center"></span>
+            <div className="experience-wrapper">
+              <div id="timeline">
+                {
+                  config.education.map(item => {
+                    return (
+                        <div className="timeline-block">
+                          <div className="timeline-content">
+                            <h2 className="position">{item.degree}</h2>
+                            <h3 className="company"><a href="{{item.url}}" target="_blank">{item.university}</a></h3>
+                          </div>
+                          <div className="timeline-img">
+                            <div className="sprite university"></div>
+                          </div>
+                          <span className="date">{item.year}</span>
+                        </div>
+                    )
+                  })
+                }
+              </div>
+              <i className="down-arrow"></i>
+            </div>
         </div>
+
+        <div className="content">
+          <h1 className="section-heading">Experience</h1>
+          <span className="divider center"></span>
+          <div className="experience-wrapper">
+            <div id="timeline">
+              {
+                config.experience.map(item => {
+                  return (
+                      <div className="timeline-block">
+                        <div className="timeline-content">
+                          <h2 className="position">{item.role}</h2>
+                          <h3 className="company"><a href="{{item.url}}" target="_blank">{item.client}</a></h3>
+                          <p className="job-desc">{item.description}</p>
+                        </div>
+                        <div className="timeline-img">
+                          <div className="sprite industry"></div>
+                        </div>
+                        <span className="date">{item.year}</span>
+                      </div>
+                  )
+                })
+              }
+            </div>
+            <i className="down-arrow"></i>
+          </div>
+        </div>
+        </div>
+
       </section>
+
+
+
+      {/*<section className="spotlight">*/}
+      {/*  <div className="image">*/}
+      {/*    <img src={pic3} alt="" />*/}
+      {/*  </div>*/}
+      {/*  <div className="content">*/}
+      {/*    <h2>*/}
+      {/*      Augue eleifend aliquet*/}
+      {/*      <br />*/}
+      {/*      sed condimentum*/}
+      {/*    </h2>*/}
+      {/*    <p>*/}
+      {/*      Aliquam ut ex ut augue consectetur interdum. Donec hendrerit*/}
+      {/*      imperdiet. Mauris eleifend fringilla nullam aenean mi ligula.*/}
+      {/*    </p>*/}
+      {/*  </div>*/}
+      {/*</section>*/}
     </section>
 
-    <section id="three" className="wrapper style3 special">
-      <div className="inner">
-        <header className="major">
-          <h2>Accumsan mus tortor nunc aliquet</h2>
-          <p>
-            Aliquam ut ex ut augue consectetur interdum. Donec amet imperdiet
-            eleifend
-            <br />
-            fringilla tincidunt. Nullam dui leo Aenean mi ligula, rhoncus
-            ullamcorper.
-          </p>
-        </header>
-        <ul className="features">
-          <li className="icon fa-paper-plane">
-            <h3>Arcu accumsan</h3>
-            <p>
-              Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem
-              tincidunt nullam amet leo Aenean ligula consequat consequat.
-            </p>
-          </li>
-          <li className="icon solid fa-laptop">
-            <h3>Ac Augue Eget</h3>
-            <p>
-              Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem
-              tincidunt nullam amet leo Aenean ligula consequat consequat.
-            </p>
-          </li>
-          <li className="icon solid fa-code">
-            <h3>Mus Scelerisque</h3>
-            <p>
-              Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem
-              tincidunt nullam amet leo Aenean ligula consequat consequat.
-            </p>
-          </li>
-          <li className="icon solid fa-headphones-alt">
-            <h3>Mauris Imperdiet</h3>
-            <p>
-              Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem
-              tincidunt nullam amet leo Aenean ligula consequat consequat.
-            </p>
-          </li>
-          <li className="icon fa-heart">
-            <h3>Aenean Primis</h3>
-            <p>
-              Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem
-              tincidunt nullam amet leo Aenean ligula consequat consequat.
-            </p>
-          </li>
-          <li className="icon fa-flag">
-            <h3>Tortor Ut</h3>
-            <p>
-              Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem
-              tincidunt nullam amet leo Aenean ligula consequat consequat.
-            </p>
-          </li>
-        </ul>
+    <section id="skill" className="skill-spotlight wrapper alt style2">
+
+      <div className="skill-wrapper">
+      <h1 style={{textAlign: "center"}}>
+        Skills
+        <br />
+      </h1>
+
+      <div className="skill-content">
+          <div style={{padding: "2em"}}>
+            <div>Language/ Environment</div><br/>
+            <ul>
+              {config.skills.language.map(item => <li id={item.id}>{item.value}</li>)}
+            </ul>
+          </div>
+          <div style={{padding: "2em"}}>
+            <div>Web Technologies</div><br/>
+            <ul>
+              {config.skills.technology.map(item => <li id={item.id}>{item.value}</li>)}
+            </ul>
+          </div>
+          <div style={{padding: "2em"}}>
+            <div>Framework</div><br/>
+            <ul>
+              {config.skills.framework.map(item => <li id={item.id}>{item.value}</li>)}
+            </ul>
+          </div>
+          <div style={{padding: "2em"}}>
+            <div>IDE/ Tools</div><br/>
+            <ul>
+              {config.skills.tools.map(item => <li id={item.id}>{item.value}</li>)}
+            </ul>
+          </div>
+      </div>
       </div>
     </section>
 
-    <section id="cta" className="wrapper style4">
-      <div className="inner">
-        <header>
-          <h2>Arcue ut vel commodo</h2>
-          <p>
-            Aliquam ut ex ut augue consectetur interdum endrerit imperdiet amet
-            eleifend fringilla.
-          </p>
-        </header>
-        <ul className="actions stacked">
-          <li>
-            <a href="/#" className="button fit primary">
-              Activate
-            </a>
-          </li>
-          <li>
-            <a href="/#" className="button fit">
-              Learn More
-            </a>
-          </li>
-        </ul>
+
+    <section className="certification-section">
+      {/*<div className="bg" data-anchor-target="#experience-section"*/}
+      {/*     data-bottom-top="transform: translate3d(0px, -150px, 0px);"*/}
+      {/*     data-top-bottom="transform: translate3d(0px, 150px, 0px);"*/}
+      {/*></div>*/}
+      <div className="overlay-wrapper">
+        <div className="certification-content">
+          <h1 className="section-heading">Certifications</h1>
+          <span className="divider center"></span>
+          <div className="certification-wrapper">
+            <ul>
+              {config.certification.map(item => <li id={item.id} className="certificate-logo">{item.name}</li>)}
+            </ul>
+
+          </div>
+        </div>
       </div>
     </section>
   </Layout>
